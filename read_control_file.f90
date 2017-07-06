72015088d5e92be7bd91316b336448abd31a5d9a
Steve Regan <rsregan@usgs.gov>
2017-07-06 15:43:52 -0600

    added xyFileName as a control parameter

--- read_control_file.f90
+++ read_control_file.f90
@@ -13,7 +13,7 @@
      &      Dyn_covtype_flag, Dyn_potet_flag, Dyn_transp_flag, Dyn_soil_flag, Dyn_radtrncf_flag, Dyn_transp_on_flag, &
      &      Dyn_sro2dprst_perv_flag, Dyn_sro2dprst_imperv_flag, Dyn_fallfrost_flag, &
      &      Dyn_springfrost_flag, Dyn_snareathresh_flag, Dyn_covden_flag, Segment_transferON_OFF, Gwr_transferON_OFF, &
-     &      Lake_transferON_OFF, External_transferON_OFF, Dprst_transferON_OFF, basinOutON_OFF, mappingFileName
+     &      Lake_transferON_OFF, External_transferON_OFF, Dprst_transferON_OFF, basinOutON_OFF, mappingFileName, xyFileName
         USE GSFMODFLOW, ONLY: Modflow_name, Modflow_time_zero
         USE PRMS_CLIMATE_HRU, ONLY: Precip_day, Tmax_day, Tmin_day, Potet_day, Transp_day, Swrad_day, &
      &      Cbh_check_flag, Cbh_binary_flag, Windspeed_day, Humidity_day
@@ -486,8 +486,13 @@
       Control_parameter_data(i)%data_type = 4
       i = i + 1
       Control_parameter_data(i)%name = 'mappingFileName'
-      mappingFileName = 'mappingFileName'
+      mappingFileName = 'MODSIM.map'
       Control_parameter_data(i)%values_character(1) = mappingFileName
+      Control_parameter_data(i)%data_type = 4
+      i = i + 1
+      Control_parameter_data(i)%name = 'xyFileName'
+      xyFileName = 'MODSIM.xy'
+      Control_parameter_data(i)%values_character(1) = xyFileName
       Control_parameter_data(i)%data_type = 4
       i = i + 1
       Control_parameter_data(i)%name = 'csv_output_file'
